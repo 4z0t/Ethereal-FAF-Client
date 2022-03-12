30de26e8d3cf58f42987062d174f5296a4a9a8ae
4z0t <gastec139@gmail.com>
2022-03-12 22:14:50 +0300

    vault init

---
+++ beta/Models/Vault/VaultEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beta.Models.Vault
+{
+    class VaultEntry
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+    }
+
+
+    class MapEntry : VaultEntry
+    {
+    }
+
+    class ModEntry : VaultEntry
+    {
+    }
+}
